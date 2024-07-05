e45420c6518af4201374374a8bd16733812259bb
johannah-pik <89136160+johannah-pik@users.noreply.github.com>
2024-07-05 12:46:12 +0200

    Add marking of variable with "edge" to prevent duplicates

--- R/supportFunctions.R
+++ R/supportFunctions.R
@@ -18,3 +18,49 @@ prepareForREMIND <- function(dt, demScen, SSPscen, transportPolScen) {          
   setcolorder(dt, c("region", "period", "GDP_scenario", "DEM_scenario", "EDGE_scenario", cols, "value"))
   return(dt)
 }
+
+#'Renames vertain variables to prevent duplicates for variables that are reported also in remind2
+#'
+#' @param vars MIF variables
+#' @returns MIF variables that are renamed to "Transport edge" where necessary
+#' @author Johanna Hoppe
+#' @import data.table
+#'
+renameDuplicateVariables <- function(vars) {
+
+  #Duplicates: Variables that are reported as well by remind2 for the GAMS side of transport
+  # in order to prevent duplicates the variables need to be renamed here
+  # After the variable harmonization fix between REMIND/EDGE-T is in place, this can be moved and instead the remind2 variables should be renamed
+
+  varsToRename <- c("ES|Transport|Road",
+                    "ES|Transport|Pass",
+                    "ES|Transport|Freight",
+                    "FE|Transport|Pass",
+                    "FE|Transport|Freight",
+                    "FE|Transport",
+                    "FE|Transport|Pass|Liquids",
+                    "FE|Transport|Pass|Hydrogen",
+                    "FE|Transport|Pass|Gases",
+                    "FE|Transport|Pass|Electricity",
+                    "FE|Transport|Freight|Liquids",
+                    "FE|Transport|Freight|Hydrogen",
+                    "FE|Transport|Freight|Gases",
+                    "FE|Transport|Freight|Electricity",
+                    "FE|Transport|Liquids",
+                    "FE|Transport|Hydrogen",
+                    "FE|Transport|Gases",
+                    "FE|Transport|Electricity",
+                    "UE|Transport|Pass|Electricity",
+                    "UE|Transport|Freight|Electricity",
+                    "UE|Transport|Electricity",
+                    "UE|Transport|Pass|Hydrogen",
+                    "UE|Transport|Freight|Hydrogen",
+                    "UE|Transport|Hydrogen",
+                    "UE|Transport|Pass|Liquids",
+                    "UE|Transport|Freight|Liquids",
+                    "UE|Transport|Liquids")
+
+  vars[variable %in% varsToRename, variable := gsub("Transport", "Transport edge", variable)]
+
+  return(vars)
+}
