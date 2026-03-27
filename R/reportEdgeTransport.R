4ac7d146731f9e419420bd0b88c0bda9fc2351d1
Johanna Hoppe <89136160+johannah-pik@users.noreply.github.com>
2026-03-27 16:52:30 +0100

    bugfix

--- R/reportEdgeTransport.R
+++ R/reportEdgeTransport.R
@@ -250,10 +250,11 @@ reportEdgeTransport <- function(folderPath = file.path(".", "EDGE-T"), data = NU
         test <- merge(reporting, REMINDvars, by = intersect(names(reporting), names(REMINDvars)))
         test[, deviationAbsolute := abs(REMINDval - value)]
         test[, deviationRelativeToREMIND := abs(REMINDval - value) / REMINDval]
+
         #Add relative deviation of FE|Transport (including bunkers) to the REMIND.mif as an indicator
         FEratio <- test[REMINDvar == "FE|Transport"]
         FEratio[, variable := "FE|Transport|a - relative deviation of EDGE-T from REMIND prior to harmonization"]
-        FEratio[, c("value", "REMINDvar", "deviationAbsolute") := NULL]
+        FEratio[, c("value", "REMINDvar", "deviationAbsolute", "REMINDval") := NULL]
         setnames(FEratio, "deviationRelativeToREMIND", "value")
         #Exclude data after 2100 from the harmonization as edget only runs until 2100
         test <- test[period <= 2100]
